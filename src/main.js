9e6a479ebd7ea66903bfe5594459343044753aa0
UI Author <ljones4@gmail.com>
2024-12-20 16:46:18 -0500

    electron test

--- src/main.js
+++ src/main.js
@@ -1,15 +1,32 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow () {
-  const win = new BrowserWindow({
+function createWindow() {
+  const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
-  win.loadFile('index.html');
+  mainWindow.loadFile(path.join(__dirname, 'build', 'index.html'));
+
+  // Open the DevTools (optional)
+  mainWindow.webContents.openDevTools();
 }
 
 app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
